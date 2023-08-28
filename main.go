1711f3beb7c5956e403f2795a30111634d92e7cd
SiirRandall <sirrandallgaming@gmail.com>
2023-08-28 02:18:33 -0400

    Add code for the Body and method type

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"sort"
 	"strings"
 	"time"
 
@@ -33,36 +34,124 @@ func main() {
 	detailsView := tview.NewTextView()
 	detailsView.SetDynamicColors(true)
 	detailsView.SetScrollable(true)
-	detailsView.SetBorder(true)
+	detailsView.SetBorder(false)
 	detailsView.SetTitle("Details")
 
+	// window1 := tview.NewTextView().
+	// 	SetDynamicColors(true).
+	// 	SetScrollable(true).
+	// 	SetBorder(true).
+	// 	SetTitle("Window 1")
+	//
+	// window2 := tview.NewTextView().
+	// 	SetDynamicColors(true).
+	// 	SetScrollable(true).
+	// 	SetBorder(true).
+	// 	SetTitle("Window 2")
+
+	urlForm := tview.NewForm().
+		AddInputField("URL", "https://jsonplaceholder.typicode.com/posts", 100, nil, nil)
+	urlForm.SetBorder(false).SetTitle("URL")
+
 	var form *tview.Form
+	var detailsForm *tview.Form
+	//	logVisible := false
 
 	app.EnableMouse(true)
 
+	detailsForm = tview.NewForm().
+		AddDropDown("Method", []string{"GET", "POST", "PUT", "DELETE"}, 0, nil).
+		AddInputField("Headers", "", 50, nil, nil).
+		AddInputField("Body", "", 50, nil, nil)
+	detailsForm.SetBorder(true).SetTitle("Request Details")
 	// Initialize the form after logView, so we can use logView in the button's function.
 	form = tview.NewForm().
-		AddInputField("URL", "https://jsonplaceholder.typicode.com/posts", 100, nil, nil).
-		AddButton("Fetch JSON", func() {
-			url := form.GetFormItem(0).(*tview.InputField).GetText()
+		// /	AddInputField("URL", "https://jsonplaceholder.typicode.com/posts", 100, nil, nil).
+
+		// Create two new input fields for the `Headers` and `Body`.
+
+		AddButton("Send", func() {
+			url := urlForm.GetFormItem(0).(*tview.InputField).GetText()
+			_, methodText := detailsForm.GetFormItem(0).(*tview.DropDown).GetCurrentOption()
+			headers := detailsForm.GetFormItem(1).(*tview.InputField).GetText() // For now, this is a simple string; you might want to split headers by line and then by ": " to get a map of header names to values.
+			requestBody := detailsForm.GetFormItem(2).(*tview.InputField).GetText()
+
+			logMessage(logView, fmt.Sprintf("Using method: %s", methodText))
+			logMessage(logView, fmt.Sprintf("Headers: %s", headers))
+			logMessage(logView, fmt.Sprintf("Body: %s", requestBody))
+
+			detailsView.SetText(
+				fmt.Sprintf("Method: %s\nHeaders: %s\nBody: %s", methodText, headers, requestBody),
+			)
+
+			req := fasthttp.AcquireRequest()
+			resp := fasthttp.AcquireResponse()
+			defer fasthttp.ReleaseRequest(req)
+			defer fasthttp.ReleaseResponse(resp)
+
+			req.SetRequestURI(url)
+			req.Header.SetMethod(methodText)
+			// For the sake of simplicity, I won't set headers here. You can parse the headers string and set them using req.Header.Set().
+			req.SetBodyString(requestBody)
+
+			headersInput := detailsForm.GetFormItem(1).(*tview.InputField).GetText()
+			headerLines := strings.Split(headersInput, "\n")
+
+			for _, line := range headerLines {
+				parts := strings.SplitN(line, ": ", 2)
+				if len(parts) == 2 {
+					headerName := parts[0]
+					headerValue := parts[1]
+					req.Header.Set(headerName, headerValue)
+				}
+			}
+
+			err := fasthttp.Do(req, resp)
+			if err != nil {
+				logMessage(
+					logView,
+					fmt.Sprintf("Error making %s request to URL: %v", methodText, err),
+				)
+				return
+			}
+
 			statusCode, body, err := fasthttp.Get(nil, url)
 			if err != nil {
 				logMessage(logView, fmt.Sprintf("Error fetching URL: %v", err))
 				return
 			}
-			if statusCode != fasthttp.StatusOK {
-				textView.SetText(fmt.Sprintf("API responded with status code: %d", statusCode))
+			textView.SetText(string(body)) // Convert the byte slice to a string
+
+			rawJSONResponse := string(body)
+			textView.SetText(rawJSONResponse)
+
+			if statusCode != fasthttp.StatusOK && statusCode != fasthttp.StatusCreated {
+				// StatusCreated (201) is also a success status for POST requests
+				return
+			}
+			// var jsonData map[string]interface{} // Using a map to get a structured response
+			// err = json.Unmarshal(body, &jsonData)
+			// if err != nil {
+			// 	textView.SetText(fmt.Sprintf("Error parsing JSON: %v", err))
+			// 	return
+			// }
+			body = resp.Body()
+			var jsonData1 interface{}
+			err = json.Unmarshal(body, &jsonData1)
+			if err != nil {
+				textView.SetText(fmt.Sprintf("Error parsing JSON: %v", err))
+				textView.SetText(string(body))
 				return
 			}
 
-			var jsonData interface{}
-			err = json.Unmarshal(body, &jsonData)
-			if err != nil {
-				textView.SetText(fmt.Sprintf("Error parsing JSON: %v", err))
-				return
-			}
-			structure := visualizeJSONStructure(jsonData, "")
+			textView.SetText(string(body))
+
+			//			prettyJSON, _ := json.MarshalIndent(jsonData1, "", "    ")
+
+			structure := visualizeJSONStructure(jsonData1, "")
 			textView.SetText(structure)
+			// rawBody := resp.Body()                                            // Get the body
+			//	logMessage(logView, fmt.Sprintf("Raw Body: %s", string(rawBody))) // Log the body
 		}).
 		AddButton("Quit", func() {
 			app.Stop()
@@ -80,6 +169,20 @@ func main() {
 	)
 
 	// Input capture for textView
+
+	logMessage(logView, "Log window initialized!")
+
+	grid := tview.NewGrid().
+		SetRows(3, 0, 6).
+		// 4 units for top row, dynamic size for middle row, 6 units for bottom
+		SetColumns(35, 0, 30). // Adjust sizes based on your preference
+		SetBorders(true).
+		AddItem(urlForm, 0, 0, 1, 2, 0, 0, true).      // URL form spanning 2 columns
+		AddItem(form, 0, 2, 1, 1, 0, 0, false).        // Main form (buttons) on the right
+		AddItem(detailsForm, 1, 0, 1, 1, 0, 0, false). // Details form in the left
+		AddItem(textView, 1, 1, 1, 2, 0, 0, false).    // Spanning 2 columns for the textView
+		AddItem(logView, 2, 0, 1, 3, 0, 0, false)      // Spanning 3 columns for the logViewi
+	isVisible := false
 	textView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		// Log the captured key to logView
 		logMessage(
@@ -91,8 +194,18 @@ func main() {
 			),
 		)
 
-		// Check if Tab is pressed
+		if event.Rune() == 'l' {
+			if isVisible {
+				grid.RemoveItem(logView) // Remove logView from grid
+			} else {
+				grid.AddItem(logView, 2, 0, 1, 3, 0, 0, false) // Add logView to grid
+			}
+			isVisible = !isVisible
+			app.Draw() // Refresh the UI
+			return event
+		}
 
+		// Check if Tab is pressed
 		if event.Key() == tcell.KeyTab {
 			// If Shift modifier is present
 			if event.Modifiers() == tcell.ModShift {
@@ -106,22 +219,16 @@ func main() {
 			}
 			return nil
 		}
+
 		return event
 	})
 
-	logMessage(logView, "Log window initialized!")
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		//		log.Println("Key pressed:", event.Rune())
+		logMessage(logView, "Toggling logView visibility")
 
-	grid := tview.NewGrid().
-		SetRows(3, 0, 6). // No change here; same rows
-		SetColumns(0, 0). // Split middle pane into two equal columns
-		SetBorders(true).
-		AddItem(form, 0, 0, 1, 2, 0, 0, true). // form spans both columns
-		AddItem(detailsView, 1, 0, 1, 1, 0, 0, false).
-		// detailsView is now in the left column of the middle row
-		AddItem(textView, 1, 1, 1, 1, 0, 0, false).
-		// textView is now in the right column of the middle row
-		AddItem(logView, 2, 0, 1, 2, 0, 0, false) // logView spans both columns
-
+		return event
+	})
 	if err := app.SetRoot(grid, true).Run(); err != nil {
 		log.Fatalf("Failed to run application: %v", err)
 	}
@@ -132,27 +239,54 @@ func visualizeJSONStructure(data interface{}, indent string) string {
 	case map[string]interface{}:
 		b := &bytes.Buffer{}
 		fmt.Fprintf(b, "{\n")
+
+		// Extract and sort the keys for consistent output.
 		keys := make([]string, 0, len(v))
 		for key := range v {
 			keys = append(keys, key)
 		}
-		for _, key := range keys {
+		sort.Strings(keys) // Sorting the keys
+
+		for i, key := range keys {
 			valueStr := visualizeJSONStructure(v[key], indent+"  ")
-			fmt.Fprintf(b, "%s  [blue]%s[white]: %s,\n", indent, key, strings.Trim(valueStr, "\n"))
+			comma := ","
+			if i == len(keys)-1 {
+				comma = "" // No comma for the last key
+			}
+			fmt.Fprintf(b, "%s  [blue]%s[white]: %s%s\n", indent, key, strings.Trim(valueStr, "\n"), comma)
 		}
+
 		fmt.Fprintf(b, "%s}", indent)
 		return b.String()
+
 	case []interface{}:
 		b := &bytes.Buffer{}
 		fmt.Fprintf(b, "[\n")
-		for _, item := range v {
+
+		for i, item := range v {
 			itemStr := visualizeJSONStructure(item, indent+"  ")
-			fmt.Fprintf(b, "%s  %s,\n", indent, strings.Trim(itemStr, "\n"))
+			comma := ","
+			if i == len(v)-1 {
+				comma = "" // No comma for the last item
+			}
+			fmt.Fprintf(b, "%s  %s%s\n", indent, strings.Trim(itemStr, "\n"), comma)
 		}
+
 		fmt.Fprintf(b, "%s]", indent)
 		return b.String()
+
 	default:
-		return fmt.Sprintf("%v", v)
+		// Apply custom styles based on the type of the value
+		switch t := data.(type) {
+		case string:
+			return fmt.Sprintf("[orange]\"%s\"[white]", t)
+		case float64:
+			return fmt.Sprintf("[green]%f[white]", t)
+		case int, int32, int64:
+			return fmt.Sprintf("[red]%d[white]", t)
+		default:
+			return fmt.Sprintf("%v", t)
+		}
 	}
 }
 
